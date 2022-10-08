e0d306e2f69343e4a50e2a01ff60f59ab4516786
Shristi <85734352+Shristi-007@users.noreply.github.com>
2022-10-08 23:34:13 +0530

    Update CARDS.js

--- src/components/CARDS/CARDS.js
+++ src/components/CARDS/CARDS.js
@@ -11,12 +11,13 @@ const CARDS=({show})=>{
             setIsLoading(false)
         },1500)
     },[])
-      const [isLoading,setIsLoading]=useState(true)
+      const [imagesAvail,setimagesAvail]=useState(false)
 
     useEffect(()=>{
-        setTimeout(()=>{
-            setIsLoading(false)
-        },1500)
+            if(show.programInfo.hasOwnProperty('images'))
+            {setimagesAvail(true)
+            }
+        
     },[])
     //console.log(show.programInfo);
     return <>
@@ -36,15 +37,15 @@ const CARDS=({show})=>{
             <Link to={`shows/${show.stationId}/${show.programId}`} style={{textDecoration:"none",color:"white"}}>
                 <div className="CARDS">
                     
-                    <img className="cards__img" src=${show.programInfo.images[0].url}/>
+                    <img className="cards__img" src={show.programInfo.images[0].url}/>
                     <div className="cards__overlay">
-                        <div className="card__title">{show?show.programTitle:""}</div>
+                        <div className="card__title">{show?show.programInfo.programTitle:""}</div>
                         <div className="card__runtime">
-                            {show?show.genre:""}
+                            {show?show.programInfo.genre:""}
                             
                         </div>
                         <div className="card__description">{show ? show.programInfo.longDescription.slice(0,118)+"..." : ""}</div>
-                        <div className="card__starttime">{show ? show.programInfo.startTime: ""}</div>
+                        <div className="card__starttime">{show ? show.startTime: ""}</div>
                     </div>
 
                     
@@ -54,7 +55,7 @@ const CARDS=({show})=>{
             <Link to={`shows/${show.stationId}/${show.programId}`} style={{textDecoration:"none",color:"white"}}>
                 <div className="CARDS">
                     
-                    <img className="cards__img" src=${show.programInfo.images[0].url}/>
+                    <img className="cards__img" src="https://images.unsplash.com/photo-1614518922230-f9216308655f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80"/>
                     <div className="cards__overlay">
                         <div className="card__title">{show?show.programTitle:""}</div>
                         <div className="card__runtime">
